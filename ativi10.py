922bdeedc069b3b2c43897ea28167b0ce288551b
vianaalves <148063436+vianaalves@users.noreply.github.com>
2023-11-13 09:04:50 -0300

    yttgu
    cghfgff

---
+++ ativi10.py
@@ -0,0 +1,13 @@
+#faça um programa que pergunteem que turno vc estuda .
+#peça para digita m-matutino ou v-vespetino ou n -norturno.
+
+
+
+
+
+
+
+
+
+
+
---
+++ ativi9.py
@@ -0,0 +1,28 @@
+#faça um programa q leia tres numeros e  mostre em ordem decrecente 
+n1= int(input ("digite um valor por gentileza"))
+
+n2=int(input ("digite outro valor "))
+
+n3=int(input("digite mais um valor"))
+if n1>n2>n3:
+    print(n1,n2,n3)
+elif  n1>n3>n2:
+    print(n1,n3,n2)
+elif n2>n1>n3:
+    print(n2,n1,n3)
+elif n2>n3>n1:
+   print(n2,n3,n1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
